7a2873d770aed1db9996c0c18b0bb0026d044185
Abstract CI <ci@lunarhue.com>
2026-04-30 20:59:25 +0000

    release: v0.0.8 from abstract@59722b83e85cbbcdec904edaaa73fb4ecb8e359e

--- package.nix
+++ package.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://github.com/lunarhue/metallic-flock-release/releases/download/v0.0.8/metallic-flock-linux-amd64";
-    hash = "sha256-9NVe6t98OKsc9iRHmyD0Byp4zP9KXH9rh2WrwFTmzfg=";
+    hash = "sha256-teiLlYMBDKTPg3pRD1zZHk5cg5XRwJsvQcyZD2Czvb0=";
   };
 
   dontUnpack = true;
