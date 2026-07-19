a866fb13cd88a5627ca2e88288d3ff6f9ddbd9a8
Abstract CI <ci@lunarhue.com>
2026-07-19 03:05:44 +0000

    release: v0.0.445

--- package.nix
+++ package.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, lib }:
 stdenv.mkDerivation {
   pname = "metallic-flock";
-  version = "0.0.443";
+  version = "0.0.445";
 
   src = fetchurl {
-    url = "https://github.com/lunarhue/metallic-flock-release/releases/download/v0.0.443/metallic-flock-linux-amd64";
-    hash = "sha256-OntCnyA3KwYKShqh92HiqM5cSGs/lUTwgOp08lOATSo=";
+    url = "https://github.com/lunarhue/metallic-flock-release/releases/download/v0.0.445/metallic-flock-linux-amd64";
+    hash = "sha256-DXF7SNAnib5EnkYf1m1HpJK1CHsw2vZceZ0fS3LIIhc=";
   };
 
   dontUnpack = true;
